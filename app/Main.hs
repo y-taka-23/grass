4b0976c42d2e5135f92f694d22c7f38abbce985b
TAKAHASHI Yuto <ytaka23dev@gmail.com>
2017-10-15 16:56:44 +0900

    Define states of the machine

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,30 @@
 module Main where
 
-import Lib
-
 main :: IO ()
-main = someFunc
+main = do
+    putStrLn "Hello, Grass!"
+
+data Instruction =
+      App Int Int
+    | Abs Int Code
+
+type Code = [Instruction]
+
+data SemanticObject =
+      LowerW
+    | Out
+    | In
+    | Succ
+    | Closure Code Environment
+
+type Environment = [SemanticObject]
+
+type Dump = [(Code, Environment)]
+
+initEnv :: Environment
+initEnv = [ Out, Succ, LowerW, In ]
+
+initDump :: Dump
+initDump = [ ([App 1 1], []), ([], []) ]
+
+type MachineConfig = (Code, Environment, Dump)
