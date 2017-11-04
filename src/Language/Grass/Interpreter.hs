4e7cac3b9fbc116ca24c8b25cd9e590916988e5f
TAKAHASHI Yuto <ytaka23dev@gmail.com>
2017-11-05 00:26:56 +0900

    Extract encode/decode functions

--- src/Language/Grass/Interpreter.hs
+++ src/Language/Grass/Interpreter.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE DeriveFunctor #-}
 module Language.Grass.Interpreter ( exec ) where
 
+import Language.Grass.Encoder
 import Language.Grass.Types
 
 import Control.Exception  ( catch, throwIO )
@@ -21,37 +22,6 @@ putCharAction ch = liftF $ PutChar ch ()
 
 getCharAction :: GrassAction (Maybe Char)
 getCharAction = liftF $ GetChar id
-
-boolean :: Bool -> SemanticObject
-boolean True  = Closure [Abs 2 [App 3 2]] [Closure [] []]
-boolean False = Closure [Abs 2 []] []
-
-encode :: Char -> SemanticObject
-encode ch = Closure [Abs 2 (mkCode (ord ch))] []
-    where
-        mkCode 0 = []
-        mkCode n = mkCode (n - 1) ++ [App (n + 1) 1]
-
-decode :: SemanticObject -> Maybe Char
-decode LowerW = Just 'w'
-decode (Closure [Abs 2 c] _) =
-    case evalChurch c [EInt 0, EFunc (+1)] of
-        Nothing -> Nothing
-        Just i  -> Just $ chr i
-decode _ = Nothing
-
-data Evaluable =
-      EInt  Int
-    | EFunc (Int -> Int)
-
-evalChurch :: Code -> [Evaluable] -> Maybe Int
-evalChurch []             ((EInt i):_) = Just i
-evalChurch []             _            = Nothing
-evalChurch ((Abs _ _):_)  _            = Nothing
-evalChurch ((App m n):c') s            =
-    case (atMay s (m - 1), atMay s (n - 1)) of
-        (Just (EFunc f), Just (EInt i)) -> evalChurch c' ((EInt (f i)):s)
-        _                               -> Nothing
 
 succMod255 :: Char -> Char
 succMod255 ch = chr $ (ord ch + 1) `mod` 255
