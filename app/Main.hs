7fdbff0c5c804141fceb43b490e06109ea5a3bef
TAKAHASHI Yuto <ytaka23dev@gmail.com>
2017-11-17 18:40:59 +0900

    Add a temporary interface for command line

--- app/Main.hs
+++ app/Main.hs
@@ -1,5 +1,15 @@
 module Main where
 
+import Language.Grass.Interpreter
+import Language.Grass.Parser
+
+import System.Environment ( getArgs )
+import System.Exit        ( die )
+
 main :: IO ()
 main = do
-    putStrLn "Hello, Grass!"
+    fp  <- head <$> getArgs
+    src <- readFile fp
+    case parseGrass fp src of
+        Left  err  -> die $ show err
+        Right code -> exec code
